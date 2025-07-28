88c29399bb7cac9291b8d8b6fc7edc688e7eb343
vijay Randhave <vijayrandhave77@gmail.com>
2025-07-28 22:19:10 +0530

    job Queue

--- Jobs/worker.js
+++ Jobs/worker.js
@@ -1,23 +1,23 @@
-const { Worker } = require("bullmq");
-const sendEmail = require("../mailer");
-const redisClient = require("../Redis/redisClient");
+// const { Worker } = require("bullmq");
+// const sendEmail = require("../mailer");
+// const redisClient = require("../Redis/redisClient");
 
-const worker = new Worker(
-  "sendEmail",
-  async (job) => {
-    try {
-      await sendEmail(job.data.templateName, job.data.sendTo, job.data);
-    } catch (error) {
-      console.log(error);
-    }
-  },
-  { connection: redisClient }
-);
+// const worker = new Worker(
+//   "sendEmail",
+//   async (job) => {
+//     try {
+//       await sendEmail(job.data.templateName, job.data.sendTo, job.data);
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   },
+//   { connection: redisClient }
+// );
 
-worker.on("completed", (job) => {
-  console.log(`Job ${job.id} completed`);
-});
+// worker.on("completed", (job) => {
+//   console.log(`Job ${job.id} completed`);
+// });
 
-worker.on("failed", (job, err) => {
-  console.error(`Job ${job.id} failed:`, err.message);
-});
+// worker.on("failed", (job, err) => {
+//   console.error(`Job ${job.id} failed:`, err.message);
+// });
